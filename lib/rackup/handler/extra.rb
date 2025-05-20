afe285feaae8dbc52841288df7e1ea321df25179
Alexander Burmatov <thatman@altlinux.org>
2025-05-20 16:13:39 +0300

    bundle gem

---
+++ lib/rackup/handler/extra.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "extra/version"
+
+module Rackup
+  module Handler
+    module Extra
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/rackup/handler/extra/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Rackup
+  module Handler
+    module Extra
+      VERSION = "0.1.0"
+    end
+  end
+end
